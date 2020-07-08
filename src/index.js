3b8ed7853083d4d47e34ec234d37f546b1f28a7b
jangyh <jangyh0722@naver.com>
2020-07-08 11:10:24 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,145 +1,164 @@
-import "normalize.css";
-import "./ui.scss";
-
-import $ from "jquery";
-
 $.fn.yhPopup = function (option) {
-  $(this).each(function () {
-      // $(this): 각각의 팝업
-      // $(this)가 popup이 아니거나 없을때.
-      if(!$(this).hasClass("popup-container")) return false;
-    
-      yhPopupInner($(this));
-  });
-
-  function yhPopupInner($el) {
-      // setting = defaultOption + option
-      var setting = makeSetting(),
-      // focusList = open popup focus list
-          focusList = setting.$el.find("button, input, a, textarea");
-   
-      function init() {
-          // add data yhPopupConfig
-          setting.$el.data({
-              yhPopupConfig: setting
-          });
-
-          // setting.show
-          if (setting.show) {
-              popupOpen();
-          } else {
-              popupClose();
-          }
-
-          // setting.backdrop : Dim Click
-        //팝업 내부가 아닌경우 클릭 시 닫힘
-          if (setting.backdrop) {
-              setting.$el.on("click", function (e) {
-                  if ($(e.target).hasClass("popup-container")) {
-                      e.preventDefault();
-                      popupClose();
-                  }
-              });
-          } else {
-              setting.$el.off("click");
-          }
-
-          // setting.keyboard : ESC
-          if (setting.keyboard) {
-              setting.$el.on("keydown", function (e) {
-                  if (e.keyCode === 27) {
-                      popupClose();
-                  }
-              });
-          }
-
-          // popup 내부 포커스 이동
-          focusList.last().on("keydown", function (e) {
-              if (e.keyCode === 9 && !e.shiftKey) {
-                  e.preventDefault();
-                  focusList.first().focus();
-              }
-          })
-          focusList.first().on("keydown", function (e) {
-              if (e.keyCode === 9 && e.shiftKey) {
-                  e.preventDefault();
-                  focusList.last().focus();
-              }
-          })
-
-          setting.$el.find(".btn-close").on("click", function () {
-              popupClose();
-          });
-      }
-
-      //default, html, js option setting
-      function makeSetting() {
-          var settingObj,
-              optionData = {},
-              optionScript = option;
-           
-              var defaultOption = {
+    $(this).each(function () {
+        // $(this): 각각의 팝업
+        // $(this)가 popup이 아니거나 없을때.
+        if(!$(this).hasClass("popup-container")) return false;
+      
+        yhPopupInner($(this));
+    });
+  
+    function yhPopupInner($el) { 
+        // setting = defaultOption + option
+        var defaultOption = {
                 show: false,
                 keyboard: true,
                 backdrop: true,
                 $el: $el,
                 $focus: $(":focus")
-             };
-        
+             },
+            setting = makeSetting(defaultOption),
+            // focusList = open popup focus list
+            focusList = setting.$el.find("button, input, a, textarea");
+     
+        function init() {
+            // add data yhPopupConfig
+            setting.$el.data({
+                yhPopupConfig: setting
+            });
 
-          // html data
-          //data 값 한번에 가져오기, 기존 디폴트 옵션 비교 후 최신 data로 수정
-          optionData = defaultOption;
+            // setting.show
+            if (setting.show) {
+                popupOpen();
+            } else {
+                popupClose();
+            }
 
-          // js option
-          if (option === undefined || option === "show") {
-              optionScript = {
-                  show: true
-              };
-          }else if (option === "hide") {
-              optionScript = {
-                  show: false
-              };
-          } else if (option === "toggle") {
-              optionScript = {
-                  show: !defaultOption.$el.hasClass("active")
-              };
+            // setting.backdrop : Dim Click
+          //팝업 내부가 아닌경우 클릭 시 닫힘
+            if (setting.backdrop) {
+                setting.$el.on("click", function (e) {
+                  console.log(e.target.className)
+                  if (!setting.$el.find(".pop-wrap").find("*").hasClass(e.target.className)){
+                    if(!setting.$el.find(".pop-wrap").hasClass(e.target.className)) {
+                          e.preventDefault();
+                          popupClose();
+                    }
+                    }
+                });
+            } else {
+                setting.$el.off("click");
+            }
+
+            // setting.keyboard : ESC
+            if (setting.keyboard) {
+               
+              setting.$el.on("keydown", function (e) {
+                    if (e.keyCode == 27) {
+                        popupClose();
+                    }
+                });
+            }
+
+            // popup 내부 포커스 이동
+            focusList.last().on("keydown", function (e) {
+                if (e.keyCode == 9 && !e.shiftKey) {
+                    e.preventDefault();
+                    focusList.first().focus();
+                }
+            })
+            focusList.first().on("keydown", function (e) {
+                if (e.keyCode == 9 && e.shiftKey) {
+                    e.preventDefault();
+                    focusList.last().focus();
+                }
+            })
+
+            setting.$el.find(".btn-close").on("click", function () {
+                popupClose();
+            });
+        }
+
+        //default, html, js option setting
+        function makeSetting(defaultOption) {
+            var settingObj,
+                optionData = {},
+                optionScript = option;
+
+            // html data
+            //data 값 한번에 가져오기, 기존 디폴트 옵션 비교 후 최신 data로 수정
+          if(defaultOption.$el.data("show")){
+            optionData.show = true;
+          }else if(!defaultOption.$el.data("show")){
+            optionData.show = false;
           }
+          if(defaultOption.$el.data("keyboard")){
+            optionData.keyboard = true;
+          }else if(!defaultOption.$el.data("keyboard")){
+            optionData.keyboard = false;
+          }
+          if(defaultOption.$el.data("backdrop")){
+            optionData.backdrop = true;
+          }else if(!defaultOption.$el.data("backdrop")){
+            optionData.backdrop = false
+          }
+          
+            // optionData = {
+            //     show: false,
+            //     keyboard: true,
+            //     backdrop: true,
+            //     $el: $el,
+            //     $focus: $(":focus")
+            //  }
 
-          settingObj = $.extend({}, defaultOption, optionData, optionScript);
-          return settingObj;
-      }
+            // js option
+            if (option === undefined || option === "show") {
+                optionScript = {
+                    show: true
+                };
+            }else if (option === "hide") {
+                optionScript = {
+                    show: false
+                };
+            } else if (option === "toggle") {
+                optionScript = {
+                    show: !defaultOption.$el.hasClass("active")
+                };
+            }
 
-      function popupOpen() {
-          // 미리 세팅 값을 바꾸고
-          setting.show = true;
-          // 화면을그리고 
-        //CSS addClass 사용성
-          $("body").addClass("popup-open");
-          setting.$el.addClass("active");
-          // focus 이벤트 지연
-          // https://vnthf.github.io/blog/jquery-focusout과click-event충돌/
-          setTimeout(function () {
-              focusList.first().focus();
-          }, 0);
-          // 바꾼 최종 세팅을 다시 넣고
-          setting.$el.data("yhPopupConfig", setting);
-      }
+            settingObj = $.extend({}, defaultOption, optionData, optionScript);
+            return settingObj;
+        }
 
-      function popupClose() {
-          // 미리 세팅 값을 바꾸고
-          setting.show = false;
-          // 화면을그리고 
-          $("body").removeClass("popup-open");
-          setting.$el.removeClass("active");
-          // 클릭했던 popup link로 focus
-          setting.$focus.focus();
-          // 바꾼 최종 세팅을 다시 넣고
-          setting.$el.data("yhPopupConfig", setting);
-      }
+        function popupOpen() {
+            // 미리 세팅 값을 바꾸고
+            setting.show = true;
+            // 화면을그리고 
+          //CSS addClass 사용성
+            $("body").addClass("popup-open");
+            setting.$el.addClass("active");
+            // focus 이벤트 지연
+            // https://vnthf.github.io/blog/jquery-focusout과click-event충돌/
+            setTimeout(function () {
+                focusList.first().focus();
+            }, 0);
+            // 바꾼 최종 세팅을 다시 넣고
+            setting.$el.data("yhPopupConfig", setting);
+        }
 
-      init();
-  }
+        function popupClose() {
+            // 미리 세팅 값을 바꾸고
+            setting.show = false;
+            // 화면을그리고 
+            $("body").removeClass("popup-open");
+            setting.$el.removeClass("active");
+            // 클릭했던 popup link로 focus
+            setting.$focus.focus();
+            // 바꾼 최종 세팅을 다시 넣고
+            setting.$el.data("yhPopupConfig", setting);
+        }
+
+        init();
+    }
 };
 // (function() {
 // })();
@@ -147,14 +166,17 @@ $.fn.yhPopup = function (option) {
 // }());
 // $(window).onload();
 //프로토타입
+
+//$(document).ready(function(){ }); 와 동일한 의미
 $(function () {
-  $("a[data-toggle='popup'], button[data-toggle='popup']").on("click", function (e) {
-      e.preventDefault();
-      var dataTarget = $(this).attr("data-target");
-      
-        $(dataTarget).yhPopup("toggle");
-      
-  });
+  
+    $("a[data-toggle='popup'], button[data-toggle='popup']").on("click", function (e) {
+        e.preventDefault();
+        var dataTarget = $(this).attr("data-target");
+        
+          $(dataTarget).yhPopup("toggle");
+        
+    });
 });
 
 // todo: 옵션 적용
@@ -164,3 +186,6 @@ $(function () {
 //   backdrop: true
 // });
 
+// :nth-child(2n+1){ clear: both; }
+// - window : 1q2w3e4r@
+// - Supervisor Password, User Password : 1q2w3e4r5t!
